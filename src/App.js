c73ba6ff9223a8be7cc7008ad6baef267a5e47b3
Alexjandro Mendoza <alexjandromendoza@macbook-pro.mynetworksettings.com>
2022-04-17 21:51:39 -0400

    routes working for home and about and notfound page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className='text-xl'>Hello!</h1>
+    <Router>
+       <div className='flex flex-col justify-between
+       h-screen'>
+         <Navbar />
+         <main className='continer mx-auto px-3 pb-12'>
+           <Routes>
+             <Route path='/' element={<Home />} />
+             <Route path='/about' element={<About />} />
+             <Route path='/notfound' element={<NotFound />} />
+             <Route path='/*' element={<NotFound />} />
+           </Routes>
+           </main>
+           <Footer />
+
       
-    </div>
-  );
+      
+      </div>
+
+    </Router>
+   
+  )
 }
 
 export default App;
